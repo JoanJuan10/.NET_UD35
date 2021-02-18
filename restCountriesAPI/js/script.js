479ac0e66d68689812bcd5958dc9714372916879
JoanJuan10 <iijoanjuan10@gmail.com>
2021-02-18 10:43:57 +0100

    API - Paises por bloque regional

--- restCountriesAPI/js/script.js
+++ restCountriesAPI/js/script.js
@@ -206,4 +206,24 @@ $(function () {
         });
         e.preventDefault();
     });
+    $("#countriesbyregionalblock").click(function (e) {
+        var codigo = prompt("Introduce el codigo de bloque regional a filtrar");
+        $.ajax({
+            type: "GET",
+            url: "https://restcountries.eu/rest/v2/regionalbloc/" + codigo,
+            dataType: "json",
+            headers: {
+                'Accept':'application/json',
+            },
+            contentType: 'application/x-www-form-urlencoded',
+            success: function (response) {
+                console.log(response);
+                alert("Mira la respuesta por Consola");
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+        e.preventDefault();
+    });
 });
