d610a5019d59bc741873db4fa62334f8c9b239e4
JoanJuan10 <iijoanjuan10@gmail.com>
2021-02-18 09:55:07 +0100

    API - Filtrar pais por nombre

--- restCountriesAPI/js/script.js
+++ restCountriesAPI/js/script.js
@@ -19,5 +19,25 @@ $(function () {
         e.preventDefault();
         
     });
-
+    $("#countrybyname").click(function (e) {
+        var pais = prompt("Introduce el pais en ingles");
+        $.ajax({
+            type: "GET",
+            url: "https://restcountries.eu/rest/v2/name/" + pais,
+            dataType: "json",
+            headers: {
+                'Accept':'application/json',
+            },
+            contentType: 'application/x-www-form-urlencoded',
+            success: function (response) {
+                console.log(response);
+                alert("Mira la respuesta por Consola");
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+        e.preventDefault();
+        
+    });
 });
