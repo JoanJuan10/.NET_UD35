4642485d908b852d5aa453f3966481ead7440620
JoanJuan10 <iijoanjuan10@gmail.com>
2021-02-18 09:59:48 +0100

    API - Obtener pais por nombre (todo el texto)

--- restCountriesAPI/js/script.js
+++ restCountriesAPI/js/script.js
@@ -40,4 +40,25 @@ $(function () {
         e.preventDefault();
         
     });
+    $("#countrybyname-alltext").click(function (e) {
+        var pais = prompt("Introduce el pais en ingles");
+        $.ajax({
+            type: "GET",
+            url: "https://restcountries.eu/rest/v2/name/" + pais + "?fullText=true",
+            dataType: "json",
+            headers: {
+                'Accept':'application/json',
+            },
+            contentType: 'application/x-www-form-urlencoded',
+            success: function (response) {
+                console.log(response);
+                alert("Mira la respuesta por Consola");
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+        e.preventDefault();
+        
+    });
 });
