5f2f8da902a0290775211f7c99ca44500232b7e2
JoanJuan10 <iijoanjuan10@gmail.com>
2021-02-18 10:01:33 +0100

    API - Obtener paises por codigo alpha

--- restCountriesAPI/js/script.js
+++ restCountriesAPI/js/script.js
@@ -61,4 +61,25 @@ $(function () {
         e.preventDefault();
         
     });
+    $("#countrybyalpha").click(function (e) {
+        var codigo = prompt("Introduce el codigo alpha del pais");
+        $.ajax({
+            type: "GET",
+            url: "https://restcountries.eu/rest/v2/alpha/" + codigo,
+            dataType: "json",
+            headers: {
+                'Accept':'application/json',
+            },
+            contentType: 'application/x-www-form-urlencoded',
+            success: function (response) {
+                console.log(response);
+                alert("Mira la respuesta por Consola");
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+        e.preventDefault();
+        
+    });
 });
