82f07c4c164f0a8b2299586c23404f98a35c1684
JoanJuan10 <iijoanjuan10@gmail.com>
2021-02-18 09:54:56 +0100

    API - Todos los Paises

---
+++ restCountriesAPI/js/script.js
@@ -0,0 +1,23 @@
+$(function () {
+    $("#allcountries").click(function (e) { 
+        $.ajax({
+            type: "GET",
+            url: "https://restcountries.eu/rest/v2/all",
+            dataType: "json",
+            headers: {
+                'Accept':'application/json',
+            },
+            contentType: 'application/x-www-form-urlencoded',
+            success: function (response) {
+                console.log(response);
+                alert("Mira la respuesta por Consola");
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+        e.preventDefault();
+        
+    });
+
+});
